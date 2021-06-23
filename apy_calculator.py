cd7524038f4d1ebd776aab18f2b031d8d9372583
bradmy609 <bradmy609@gmail.com>
2021-06-22 23:57:51 -0700

    Change apy format from percent to whole number for consistency with apr/deposit fee format.

--- apy_calculator.py
+++ apy_calculator.py
@@ -21,7 +21,7 @@ def calc_apy(apr, num_daily_compounds, initial_investment=100, days=365, dfee = 
         if remaining_duration != 0:
             profit += (initial_investment+profit) * remaining_duration * daily_apr
 
-    apy = (profit/original_investment) * 100
+    apy = (profit/original_investment)
     final_value = initial_investment+profit
     pool_apr = apr
     return pool_apr, profit, final_value, apy
@@ -36,7 +36,7 @@ def tres_commas(profit_string, digits):
     elif digits <= 9 and digits >= 7:
         result = "${},{},{}".format(profit_string[-digits:-6], profit_string[-6:-3], profit_string[-3:])
         return result
-        
+
 #Parameters for calc_apy function (Input apr as whole number)
 initial_investment = 100
 apr = 3
@@ -51,11 +51,11 @@ result = tres_commas(profit_as_string, digits)
 
 print('Deposit Fee: {}%'.format(deposit_fee*100))
 print('Pool APR: {}%'.format(int(pool_apr*100)))
-print('Pool APY: {}% -- (Calculated after deposit fee)'.format(int(apy)))
+print('Pool APY: {}% -- (Calculated after deposit fee)'.format(int(apy*100)))
 print('Initial investment USD: ${}.00'.format(int(initial_investment)))
 print('Days invested: {}'.format(days))
 print('Profit in USD: ${}.00'.format(int(profit)))
-print('Profit %: {}%'.format(int(apy)))
+print('Profit %: {}%'.format(int(apy*100)))
 print('Total value returned: ${}'.format(int(final_value)))
 print('')
 print('There are {} digits in the profit integer.'.format(digits))
